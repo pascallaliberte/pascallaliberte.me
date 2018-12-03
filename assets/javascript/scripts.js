871887357c0fa60eba8639a230fc5a2636533622
Pascal Laliberté <pascal@pascallaliberte.me>
2018-12-03 16:57:32 -0500

    Remove scrollreveal

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,47 +1,4 @@
 (function(){
-  if (typeof ScrollReveal == "undefined") {
-    return;
-  }
-
-  setSectionInView = function() {
-    var elInView = document.querySelector('[data-sr-revealed="true"]');
-    if (!elInView) {
-      return;
-    }
-
-    var sectionInView = elInView.getAttribute('id');
-    var htmlTag = document.querySelector('html');
-
-    if (!sectionInView) {
-      return;
-    }
-    htmlTag.setAttribute('data-section-in-view', sectionInView);
-  }
-
-  afterReveal = function(elScrolledTo) {
-    elScrolledTo.setAttribute('data-sr-revealed', true);
-    setSectionInView();
-  }
-
-  afterReset = function(elScrolledTo) {
-    elScrolledTo.setAttribute('data-sr-revealed', false);
-    setSectionInView();
-  }
-
-  addScrollReveal = function() {
-    var sr = ScrollReveal({
-      scale: 0.95,
-      opacity: 0.1,
-      distance: "50px",
-      reset: true,
-      viewFactor: 0.3,
-      delay: 300,
-      afterReveal: afterReveal,
-      afterReset: afterReset
-    });
-    sr.reveal('.scroll-reveal');
-  }
-
   makeVideosResponsive = function() {
     if (typeof reframe == "undefined") {
       return;
@@ -50,6 +7,5 @@
     reframe('iframe');
   }
 
-  addScrollReveal();
   makeVideosResponsive();
 })()
